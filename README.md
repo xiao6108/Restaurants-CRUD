2103f75b5a57929492f88961175db446c3e36c3f
YA-CHEN <xiao6108@gmail.com>
2019-04-14 23:55:55 +0800

    Add index page layout

---
+++ README.md
@@ -0,0 +1,45 @@
+# 我的餐廳清單
+
+將你有興趣的美食餐廳，填入訊息後呈現出來的網站。
+
+## Installing
+
+如何下載並啟動專案
+
+1. 點選 `Clone With HTTP` 複製 `<URL>`
+1. 開啟終端機輸入 `git clone <URL>`。
+1. 下載完成會顯示 Done 訊息。
+1. 開啟本地 MongoDB 資料庫。
+1. 從終端機導入目標檔案。
+1. 安裝 Express 套件 `npm i express`。
+1. 安裝 Handlebars 套件 `npm i express-handlebars`。
+1. 安裝 Mongoose 套件 `npm i mongoose`。
+1. 輸入 `node app.js` 開啟本地伺服器。
+1. 瀏覽器輸入 http://localhost:3000 。
+1. 網頁即可運作並執行動作。
+
+## Features
+
+特點
+
+- 可瀏覽全部的餐廳
+
+* 點擊 `添加您喜愛的餐廳` 可填入一家餐廳的各式資料，即可呈現在頁面
+
+- 點擊圖片可瀏覽一家餐廳的詳細資訊
+
+* 點擊 `編輯` 可修改一家餐廳的資料
+
+- 點擊 `刪除` 可移除一家餐廳
+
+* 搜尋欄可輸入餐廳名關鍵字，進行查找
+
+## Built With
+
+- [Express](https://expressjs.com/zh-tw/starter/installing.html) - Node.js web framework
+- [Handlebars](https://www.npmjs.com/package/express-handlebars) - Template Engine
+- [Mongoose](https://www.npmjs.com/package/mongoose) - MongoDB Model
+
+## Contributor
+
+[馬振壹 Wanaka](https://github.com/asd8116)
--- app.js
+++ app.js
@@ -2,20 +2,22 @@
 const express = require('express')
 // 啟用 express
 const app = express()
+// 啟用 express-handlebars
+const exphbs = require('express-handlebars')
+// 啟用 body-parser
+const bodyParser = require('body-parser')
 // 載入 mongoose
 const mongoose = require('mongoose')
 // 設定連線到 mongoDB
 // 加上 { useNewUrlParser: true }
-mongoose.connect('mongodb://localhost/restaurant',{ useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
 
 // mongoose連線後拿到 Connection 的物件
 const db = mongoose.connection
-
 // 連線異常
-db.on('error', ()=>{
+db.on('error', () => {
   console.log('mongodb error!')
 })
-
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
@@ -24,10 +26,19 @@ db.once('open', () => {
 // 載入 restaurant model
 const Restaurants = require('./models/restaurants')
 
-// 設定第一個首頁路由
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+//Setting static file
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// 列出全部 restaurant 頁面
 app.get('/', (req, res) => {
-  res.send('hello world')
-})
+  Restaurants.find((err, data) => {
+    if (err) return console.error(err);
+    return res.render('index', { data })
+  })
+});
 
 // 設定 express port 3000
 app.listen(3000, () => {
